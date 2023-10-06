43a96c61c5d09ce3b1967ef44e692725338da21e
Redi Lleshi <redilleshi52@gmail.com>
2023-10-06 11:14:35 -0400

    Sign Up Logic And Functionality
    Added files:
    - SignUpController.cs
    - UserRegisterDto.cs
    The above files allow for the functionality of registering the new users from the app.

---
+++ Dtos/UserRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SharkValleyServer.Dtos
+{
+    public class UserRegisterDto
+    {
+        [EmailAddress]
+        [Required]
+        public string? Email { get; set; } = "";
+
+        [Required]
+        public string Password { get; set; } = "";
+    }
+}
